746d3db1456def3ea71d5aa1477decf18a90d9bf
chpill <etienne@spillemaeker.com>
2017-02-24 17:16:50 +0100

    Glorious initial commit

---
+++ env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [bacon.android.core :as core]))
+
+ (core/init)
+
+
---
+++ test/bacon/core_test.clj
@@ -0,0 +1,7 @@
+(ns bacon.core-test
+  (:require [clojure.test :refer :all]
+            [bacon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
